c3df07f94713ccff1972712c063186c2aef9de10
ramoli-git <amoli.raj@gmail.com>
2025-11-25 09:40:22 -0800

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author= 'Raj Amoli',
+    author_email= 'amoli.raj@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+) 
